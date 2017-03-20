f3c42381e0d18058823b53aa0dc0e36a251dea65
Aswinprabhakaran <aswinp96@gmail.com>
2017-03-20 16:42:25 +0530

    Add files via upload

---
+++ beg-p1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+printf("enter the number:");
+scanf("%d",&num);
+if (num==0)
+printf("the number is :0");
+if(num>0)
+printf("the number is:positive");
+if(num<0)
+printf("the number is :negative");
+}
