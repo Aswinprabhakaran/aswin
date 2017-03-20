ffb14763bf4c046dd9b4e85e70e283bba1645d74
Aswinprabhakaran <aswinp96@gmail.com>
2017-03-20 16:59:45 +0530

    Add files via upload

---
+++ beg-p3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char var,a,e,i,o,u,A,E,I,O,U;
+printf("enter the variable:");
+scanf("%c",&var);
+if((var=='a')||(var=='e')||(var=='i')||(var=='o')||(var=='u')||(var=='A')||(var=='E')||(var=='I')||(var=='O')||(var=='U'))
+printf("the variable is an VOWEL");
+else
+printf("the variable is an consonant");
+}
