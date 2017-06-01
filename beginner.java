30cfedff59512b9aada8ca07cc9b3c254a61c9ca
Aswinprabhakaran <aswinp96@gmail.com>
2017-06-01 15:17:17 +0530

    Add files via upload

---
+++ beginner.java
@@ -0,0 +1,11 @@
+class beinner
+{
+public static void main(String args[])
+{
+char ch=args[0].charAt(0);
+if(Character.isLetter(ch))
+{System.out.println(" A L P H A B E T ");}
+else
+{System.out.println(" NOT AN A L P H A B E T ");}
+}
+}
