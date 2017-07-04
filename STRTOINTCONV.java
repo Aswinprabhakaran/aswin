377d57dfbe4ee3d73dca74497f866d1e590643b6
Aswinprabhakaran <aswinp96@gmail.com>
2017-07-04 13:49:13 +0530

    Add files via upload

---
+++ STRTOINTCONV.java
@@ -0,0 +1,16 @@
+package PLAYER;
+
+import java.util.Scanner;
+
+public class STRTOINTCONV {
+
+	
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the string:");
+	String str=sc.next();
+	int c=Integer.parseInt(str);
+	System.out.println(c);
+	}
+
+}
