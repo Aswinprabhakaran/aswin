6140462ae351a5e69bf5538f8bea046428d4cd5f
Aswinprabhakaran <aswinp96@gmail.com>
2017-07-21 15:55:15 +0530

    Add files via upload

---
+++ set1_01.java
@@ -0,0 +1,21 @@
+package Hunter;
+import java.util.Scanner;
+public class set1_01 {
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the size of the array:");
+int size=sc.nextInt();
+int[] arr=new int[size];
+for(int i=0;i<size;i++){
+	arr[i]=sc.nextInt();
+}
+for(int i=0;i<size;i++){
+	for(int j=i+1;j<size;j++){
+	if(arr[i]==arr[j]){
+		System.out.println("the duplicate is "+arr[j]);
+	}
+	}
+}
+	}
+
+}
